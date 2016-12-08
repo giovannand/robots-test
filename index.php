4dcc018f6f4a7bc003b843919b49eade036f9d05
giovannand <giovanna.nd7@gmail.com>
2016-12-08 12:18:42 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+use Telegram\Bot\Api;
+
+use Telegram\Bot\Keyboard\Keyboard;
+
+use Telegram\Bot\Helpers\Emojify;
+
+use Telegram\Bot\FileUpload\InputFile;
+
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+
+$dotenv->load();
